4d112007046c5b64c01dc71012d4b8a7f956aebb
FQ <80850154+FQuental@users.noreply.github.com>
2025-10-01 17:14:47 -0300

    binaryDecode_att3-ready

--- Lecture0809/design/decodificador_binario.v
+++ Lecture0809/design/decodificador_binario.v
@@ -1,16 +1,14 @@
-module decodificador_binario #(parameter N = 4, parameter M = $clog2(N)) (
+module decodificador_binario #(
+    parameter N = 4,
+    parameter M = 1 << N
+  )(
     input  wire [N-1:0] in,
     output reg  [M-1:0] out
   );
-
-  integer i;
   always @(*)
   begin
-    out = 0; // valor default
-    for (i = 0; i < M; i = i + 1)
-    begin
-      if (in[i])
-        out = i[M-1:0];
-    end
+    out = {M{1'b0}};
+    out[in] = 1'b1;
   end
+
 endmodule
